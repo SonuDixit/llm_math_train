1237673d16e309db2a89038775be5b4fee27215d
sonu.dixit <sonudixit2k@gmail.com>
2025-02-05 11:15:00 +0000

    exp modify

--- grpo_train.py
+++ grpo_train.py
@@ -26,8 +26,8 @@ def format_reward_func(completions, **kwargs):
     matches = [re.match(pattern, content) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
 
-exp_id:int = 5
-exp_name:str = 'test_grpo_bp_2'
+exp_id:int = 10
+exp_name:str = 'first_exp'
 model_name:str = "Qwen/Qwen2-0.5B"
 train_size:int = 327680
 test_size:int = 1024
@@ -35,15 +35,16 @@ test_size:int = 1024
 train_ds, test_ds = get_dataset(train_size=train_size, 
                                 test_size=test_size)
 print(f'datasets training:{train_ds}, validation:{test_ds}')
+
 training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", 
                            logging_steps=1,
                            per_device_train_batch_size=1,
-                           gradient_accumulation_steps=1,
-                           num_generations=4,               # G in GRPO paper
+                           gradient_accumulation_steps=32,
+                           num_generations=16,               # G in GRPO paper
                            learning_rate=1e-5,
-                           max_completion_length=32,        # |o_i| in GRPO paper
-                           run_name=f'{exp_id}_{exp_name}',                 # wandb logging
-                           use_cpu=True,
+                           max_completion_length=512,        # |o_i| in GRPO paper
+                           run_name=f'{exp_id}_{exp_name}',  # wandb logging
+                           use_cpu=False,                    # cpu for local mps device
                            num_train_epochs=1,
                            )
 trainer = GRPOTrainer(
