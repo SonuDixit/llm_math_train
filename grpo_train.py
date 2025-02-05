02a3c533b10f233307cbbbe122db56a241170a04
sonu_dixit <sonu@Sonus-MacBook-Air.local>
2025-02-05 15:20:24 +0530

    new exp

--- grpo_train.py
+++ grpo_train.py
@@ -1,4 +1,5 @@
-import re
+import re, torch
+import random
 from trl import GRPOConfig, GRPOTrainer
 
 from data_process import get_dataset
@@ -10,8 +11,8 @@ def accuracy_reward_func(prompts, completions, **kwargs):
     solutions = [completion[0]["content"] for completion in completions]
     ground_truths = kwargs.get('ground_truth')
     # print(ground_truths)
-    print('accuracy reward')
-    print(solutions, ground_truths)
+    # print('accuracy reward')
+    # print(solutions, ground_truths)
     scores = [compute_score(solution_str=solution, 
                             ground_truth=ground_truth) for solution, ground_truth in zip(solutions, ground_truths)]
     return scores
@@ -20,16 +21,19 @@ def format_reward_func(completions, **kwargs):
     """Reward function that checks if the completion has a specific format."""
     pattern = r"^<think>.*?</think><answer>.*?</answer>$"
     completion_contents = [completion[0]["content"] for completion in completions]
-    print('format reward')
-    print(completion_contents)
+    # print('format reward')
+    # print(completion_contents)
     matches = [re.match(pattern, content) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
 
-exp_id:int = 4
-exp_name:str = 'exp'
+exp_id:int = 5
+exp_name:str = 'test_grpo_bp_2'
 model_name:str = "Qwen/Qwen2-0.5B"
+train_size:int = 327680
+test_size:int = 1024
 
-train_ds, test_ds = get_dataset()
+train_ds, test_ds = get_dataset(train_size=train_size, 
+                                test_size=test_size)
 print(f'datasets training:{train_ds}, validation:{test_ds}')
 training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", 
                            logging_steps=1,
@@ -38,7 +42,9 @@ training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO",
                            num_generations=4,               # G in GRPO paper
                            learning_rate=1e-5,
                            max_completion_length=32,        # |o_i| in GRPO paper
-                           run_name=f'{exp_id}_{exp_name}'                 # wandb logging
+                           run_name=f'{exp_id}_{exp_name}',                 # wandb logging
+                           use_cpu=True,
+                           num_train_epochs=1,
                            )
 trainer = GRPOTrainer(
     model=model_name,
